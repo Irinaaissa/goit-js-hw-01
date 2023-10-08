cf4096a66bdec89d7ae2731b34cb11965547892e
Irinaaissa <irinaalisa7506@gmail.com>
2023-10-08 20:25:33 +0300

    12

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
-let makeTransaction = quantity;
-makeTransaction= pricePerDroid;
-const totalPrice= pricePerDroid*quantity;
-const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+function makeTransaction(quantity, pricePerDroid) {
+    var totalPrice = quantity * pricePerDroid;
+    return "You ordered " + quantity + " droids worth " + totalPrice + " credits!";
+  }
+makeTransaction(5, 3000);
+makeTransaction(3, 1000);
+makeTransaction(10, 500);
